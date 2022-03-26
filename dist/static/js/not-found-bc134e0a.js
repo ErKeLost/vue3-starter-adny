ec6678cc5c32e470e93ff5f3111db3c522d19f81
erkelost <1256029807@qq.com>
2022-03-26 20:09:30 +0800

    chore: build with legacy

---
+++ dist/static/js/not-found-bc134e0a.js
@@ -0,0 +1,1 @@
+import{p as s,f as o,o as a,q as t,W as e}from"./vendor-366ed904.js";const n=s({setup(s){const n=o("not found");return(s,o)=>(a(),t("div",null,e(n.value),1))}});export{n as default};
